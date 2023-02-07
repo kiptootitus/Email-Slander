a796e8682d3bf9f886c743871ed0a312f5e56da9
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-07 19:43:17 +0300

    updates

--- myemail/apps.py
+++ myemail/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyEmaiSenderConfig(AppConfig):
+class MyemailConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_emai_sender'
+    name = 'myemail'
