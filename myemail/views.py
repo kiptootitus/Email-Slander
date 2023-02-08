544de34164eb7ab6f89e764b83f44231984d0b0c
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-08 16:18:33 +0300

    updates

--- myemail/views.py
+++ myemail/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def mysite(request):
-    return HttpResponse("Hello, world. You're at the myemail index.")
+    template = loader.get_template('mysite.html')
+    return HttpResponse(template.render())
